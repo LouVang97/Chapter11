326075971bd78be88f56297ac193ca13764f024d
LouVang97 <35350104+LouVang97@users.noreply.github.com>
2018-04-04 21:00:32 -0700

    Update Ch11.sh

--- Ch11.sh
+++ Ch11.sh
@@ -8,4 +8,22 @@ echo "Looking for all entries in /ect/password that match the regular expression
 echo 
 grep r.*t /ect/password 
 
+echo "Using no qoute"
+echo $245
 
+echo "Using single quote"
+echo '$245'
+
+echo "Using daouble quotes"
+echo "$245"
+
+# working with conditionals 
+echo "Solve the following question:\n"
+
+echo "What is 3+3?"
+read ANSWERx
+if [ "Answer" = 6 ]; then
+      echo "Correct"
+else 
+      echo "Wrong"
+fi
